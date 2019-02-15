2c16bde7d69b7aabb870ffe5beac4128c931bf38
misigno <efournier92@gmail.com>
2019-02-14 20:24:46 -0500

    integrate typed.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    var typed = new Typed('.type-my-name', {
+      strings: ["E Fournier"],
+      typeSpeed: 90,
+    });
+    typed.start();
+  }
 }
